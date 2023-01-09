b7754de436f43d9c00cc0b94f9fa49486e64bf14
viz-nu <vishnu.teja101.vt@gmail.com>
2023-01-09 21:45:08 +0530

    9/150

--- Leetcode/easy.js
+++ Leetcode/easy.js
@@ -1,4 +1,4 @@
-//roman to int
+// 13. Roman to Integer
 //I             1
 // V             5
 // X             10
@@ -9,29 +9,147 @@
 
 
 
-romanToInt("LXVIV");
+// romanToInt("LXVIV");
 
 
-function romanToInt(s) {
-    const ref = [
-        'I', 1,
-        'V', 5,
-        'X', 10,
-        'L', 50,
-        'C', 100,
-        'D', 500,
-        'M', 1000
-    ];
-    const a = s.split("");
-    let b = a.map(ele => ref[ref.indexOf(ele) + 1]);
-    b.push(0)
-    console.log(b)
-    let sum = 0;
-    for (let i = 0; i < b.length - 1; i++) {
+// function romanToInt(s) {
+//     const ref = {
+//         "O": 0,
+//         'I': 1,
+//         'V': 5,
+//         'X': 10,
+//         'L': 50,
+//         'C': 100,
+//         'D': 500,
+//         'M': 1000
+// };
+//     const a = s.split("");
+//     a.push("O");
 
-        if (b[i] >= b[i + 1]) sum = sum + b[i]; //3,65
-        else sum = sum - b[i];
-    };
-    console.log(sum);
-};
-//[ 50, 10, 5, 1, 5, 0 ]
+//     let sum = 0;
+//     for(let i=0;i<a.length-1;i++) {
+//         if (ref[a[i]]>=ref[a[i+1]]) sum = sum + ref[a[i]]
+//         else sum = sum - ref[a[i]]
+
+//     };
+
+//     return (sum);
+// };
+
+
+
+
+// 14. Longest Common Prefix
+
+
+// function longestCommonPrefix(strs) {
+//     strs.sort()
+//     let com=""
+//     if(!strs[1]){return strs[0]}
+//     for (let i = 0,j=0; i < strs[j].length; i++) {                                     
+//         let element = strs[j][i];                                                     
+//         c=com                                                                        
+//         for (let k = 1; k < strs.length && strs[k][i]===element; k++) {       
+//              if (k==(strs.length-1)) {com+=element; }                               
+//         }
+//         if(c==com){break}                                                            
+//     }
+//     return com;                                                                      
+// };
+// console.log(longestCommonPrefix(["f", "ff", "fff"]));
+
+
+// 20. Valid Parentheses
+
+// function isValid(s) {
+//     s = s.split("")
+//     const openers = "({[", closers = ")}]"
+//     for (let i = 0; s.length; i++) {
+//         if (openers.indexOf(s[i])>=0) {
+//             continue;  
+//         }
+//         else if (closers.indexOf(s[i])>=0  && (openers.indexOf(s[i - 1]) == closers.indexOf(s[i]))) {
+//             s.splice(i - 1, 2);
+//             i=0;
+
+//         }
+//         else {break}
+
+//     }
+
+//     if (!s[0]) return true
+//     else return false
+// }
+
+// console.log(isValid("(}"))
+
+
+
+
+
+
+// 88. Merge Sorted Array
+// var merge = function (nums1, m, nums2, n) {
+//     nums1.push(...nums2);
+//     nums1.sort((a, b) => a - b);
+//     for (let i = 0; i < m+n; i++) {
+//        if(nums1[i]===0){
+//         nums1.splice(i,1);
+//         i--;
+//        }
+        
+//     }
+    
+//     //   nums1 =nums1.sort((a,b) => a-b).filter(item => item>0)
+//     return (nums1);
+// };
+// console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 100, 54], 3));
+
+// 1. Two Sum
+// const twoSum = function(nums, target) {
+//     let a=[]
+// nums.forEach((element,index)=> {
+//         for (let i = index+1; i < nums.length; i++) {
+//             if (element+nums[i]==target) {a.push(index,i)}
+//         }
+
+//     })
+//     return a
+// };
+// console.log(twoSum([2,7,2,1,8,6,3,11,15],9))
+
+
+// 9. Palindrome Number
+// const isPalindrome = function(x) {
+//     x=x.toString()
+//     let y=x.split("").reverse().join("")
+
+//     return  (x===y) ? true :false
+    
+// };
+// console.log(isPalindrome(54322345));
+
+
+
+
+// 26. Remove Duplicates from Sorted Array
+
+// const removeDuplicates = function(nums) {
+//    var nums = [...new Set(nums)];
+//     return nums.length
+//   };
+// const removeDuplicates = function(nums) {
+//   return nums.filter((item,index) =>{ if(nums.indexOf(item)===index){return item}})
+// };
+// const removeDuplicates = function(nums) {
+//     let a=[]
+//    for(let i=0;i<nums.length;i++){
+//        if (nums.indexOf(nums[i])===i){
+//            a.push(nums[i])
+//        }
+//    }
+//    return a
+// };
+
+
+// console.log(removeDuplicates(nums));
---
+++ Leetcode/hard.js
@@ -0,0 +1,8 @@
+// 4. Median of Two Sorted Arrays
+
+// const findMedianSortedArrays = function (nums1, nums2) {
+//     nums1= nums1.concat(nums2).sort((item1,item2) => item1 - item2)
+//     return (nums1.length%2===1)? nums1[Math.floor(nums1.length/2)]:(nums1[(nums1.length/2)]+nums1[(nums1.length/2)-1])/2
+// };
+// console.log(findMedianSortedArrays([1, 4, 6, 9, 3], [2, 6,0, 8])); 
+
