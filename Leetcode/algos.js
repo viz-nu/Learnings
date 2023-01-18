4ae045b52c5f120cb822b7e8463a75eee98e548b
viz-nu <vishnu.teja101.vt@gmail.com>
2023-01-18 12:21:47 +0530

    algos

--- Leetcode/algos.js
+++ Leetcode/algos.js
@@ -22,7 +22,7 @@
 //     a=[]
 //   for(let i=0;i<s;i++){
 //       if(str1[i]) a.push(str1[i])
-      
+
 //       if(str2[i]) a.push(str2[i])
 //   }
 //   return a.join("")
@@ -43,13 +43,70 @@
 
 // Write a program to compute the distance between the points (x1, y1) and (x2, y2)?
 
-const distance=(x1, y1,x2, y2)=>{
-  return (x1-x2)**2
-}
-console.log(distance(x1, y1,x2, y2));
+// const distance = (x1, y1, x2, y2) => {
+//   return (((x1 - x2) ** 2) + ((y1 - y2) ** 2)) ** 0.5
+// }
+// console.log(distance(0, 0, 1, 1));
+
+
 // Write a program to calculate body mass index?
+
+// const BMI = (weight, height) => {
+// return weight/(height**2)
+
+// }
+// console.log(BMI(70, 1.7));
+
+
+
 // Write a program to filter the positive numbers from a list?
+
+
+// const pos=(arr)=>{
+//   return arr.filter(ele=>ele>=0)
+// }
+
+// console.log(pos([2,4,5,-3,5,-6,-9,-43]));
+
+
+
 // Write a program to test whether a passed letter is a vowel or not?
 
 
+// const vowel=(char)=>{
+//   list="aeiou"
+//   return (list.includes(char)? "It is a vowel":"It is not a vowel" )
+// }
+// console.log(vowel("g"));
 
+
+// WAP to Check whether given year is a leap year or not?
+
+// const LeapTest=(year)=>{
+//   return (year%4==0 &&year%100!=0)? "It is a leap year":"It is not a leap year"
+// }
+// console.log(LeapTest(2000));
+
+// WAP to check whether given string is a palindrome or not?
+// const palindromeTest=(str)=>{
+//  let str1=str.split("").reverse().join("")
+
+
+//   return (str1===str)? "It is a Palindrome":"It is not a Palindrome"
+// }
+// console.log(palindromeTest("MADDAM"));
+
+
+// 3. WAP to find sum of all the digits of given input number?
+
+
+// const sumOfdigits=(num)=>{
+// num+=""
+//   arr=num.split("")
+
+//   return arr.reduce((acc,ele)=>{acc+=(ele*1)
+//     return acc
+//   },0)
+// }
+
+// console.log(sumOfdigits(54321));
