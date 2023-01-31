68968bd3652d97f6605f79bfdd67842d80eb8eb0
viz-nu <vishnu.teja101.vt@gmail.com>
2023-01-31 18:24:41 +0530

    66th easy

--- Leetcode/easy.js
+++ Leetcode/easy.js
@@ -217,3 +217,13 @@
 //     else return false
 // }
 // console.log(setZeroes(a));
+
+
+// 66. Plus One
+
+
+const plusOne = function(digits) {
+
+    return  (BigInt(digits.join(""))+1n+"").split("")
+};
+console.log(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]));
